ec4a018482934cceeadaa21c3a23668d88ed03ab
Leonel Rodriguez <leoneljrodriguez@gmail.com>
2023-10-21 17:42:04 -0400

    :bug: Fixed error in nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.wcbackoffice.com",
-        port: "",
-        pathname: "",
-      },
-    ],
-  },
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
 };
 
 module.exports = nextConfig;
